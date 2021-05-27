9bb5ca49ca1b97aa30b50068e63b5088388198a7
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-05-27 23:22:34 +0200

    chore: add more config to svelte

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,34 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: [`.svelte`],
   kit: {
+    amp: false,
+    appDir: `_app`,
+    files: {
+      assets: `static`,
+      hooks: `src/hooks`,
+      lib: `src/lib`,
+      routes: `src/routes`,
+      serviceWorker: `src/service-worker`,
+      template: `src/app.html`,
+    },
+    floc: false,
+    hydrate: true,
+    paths: {
+      assets: ``,
+      base: ``,
+    },
+    prerender: {
+      crawl: true,
+      enabled: true,
+      force: false,
+      pages: [`*`],
+    },
+    router: true,
+    ssr: true,
     target: `#svelte`,
+    trailingSlash: `never`,
   },
   preprocess: preprocess(),
 };

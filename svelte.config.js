062e235c40709653d3a66c9331108797fa698257
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-05-27 23:11:47 +0200

    chore: fix linters

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,10 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    target: `#svelte`,
+  },
+  preprocess: preprocess(),
 };
 
 export default config;

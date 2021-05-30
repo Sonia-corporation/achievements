62020887e79de65b08324dee44a9e3cb61ff61dc
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2021-05-30 12:03:05 +0200

    feat(seo): add custom meta for each page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,1 +1,45 @@
+<script>
+  import { base } from '$app/paths';
+  import { dev } from '$app/env';
+
+  const metaTitle = 'Error - Achievements';
+  const metaDescription = 'Oops, an error occurred!';
+  const assetsBase = dev ? '' : base;
+  const url = `${assetsBase}/error`;
+</script>
+
+<svelte:head>
+  <!-- Common images -->
+  <link rel="icon" href="{assetsBase}/favicon/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="{assetsBase}/favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="{assetsBase}/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="{assetsBase}/favicon/android-chrome-192x192.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="{assetsBase}/favicon/favicon-16x16.png" />
+  <link rel="manifest" href="{assetsBase}/site.webmanifest" />
+  <link rel="mask-icon" href="{assetsBase}/favicon/safari-pinned-tab.svg" color="#ffb300" />
+
+  <!-- Specific meta for this page -->
+  <link href="{url}" rel="canonical" />
+  <meta content="{url}" property="og:url" />
+  <title>{metaTitle}</title>
+  <meta content="{metaTitle}" property="og:title" />
+  <meta content="{metaTitle}" property="og:image:alt" />
+  <meta content="{metaTitle}" name="twitter:title" />
+  <meta content="{metaTitle}" name="twitter:image:alt" />
+  <meta content="{metaTitle}" itemprop="name" />
+  <meta content="{metaDescription}" name="description" />
+  <meta content="{metaDescription}" property="og:description" />
+  <meta content="{metaDescription}" name="twitter:description" />
+  <meta content="{metaDescription}" itemprop="description" />
+  <meta content="{base}/favicon/prize-1280.png" property="og:image" />
+  <meta content="{base}/favicon/prize-1280.png" property="og:image:url" />
+  <meta content="{base}/favicon/prize-1280.png" property="og:image:secure_url" />
+  <meta content="{base}/favicon/prize-1280.png" name="twitter:image" />
+  <meta content="{base}/favicon/prize-1280.png" itemprop="image" />
+  <meta content="1280" property="og:image:width" />
+  <meta content="1280" property="og:image:height" />
+  <meta content="image/png" property="og:image:type" />
+  <meta content="summary_large_image" name="twitter:card" />
+</svelte:head>
+
 Error page
